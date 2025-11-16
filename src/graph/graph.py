5ea06077a283db9fec155717d0df252f4873c77b
Leonid <lemalaxz2@gmail.com>
2025-11-16 21:51:54 +0100

    graph fixed

--- src/graph/graph.py
+++ src/graph/graph.py
@@ -23,6 +23,21 @@ class ScheduleGraph:
                 return competition
         return None
 
+    def get_competitor_by_fullname(self, fullname) -> Competitor | None:
+        for competitor in self.competitors:
+            if competitor.full_name_1 == fullname or competitor.full_name_2 == fullname:
+                return competitor
+        return None
+
+
+    def get_performances_by_fullname(self, fullname) -> list[Performance]:
+        competitor = self.get_competitor_by_fullname(fullname)
+        if competitor:
+            return competitor.performances
+        else:
+            return []
+
+
     def fill_with_data(self, config_path: str):
         cfg = ConfigLoader(config_path).load()
 
@@ -31,8 +46,12 @@ class ScheduleGraph:
         jury_df = ExcelTableLoader(**cfg.files["jury"]).load()
         schedule_df = ExcelTableLoader(**cfg.files["schedule"]).load()
 
+
+        # Simple parse of competitions, they are currently not linked to any other table
         self.competitions = CompetitionParser(cfg.columns["competitions"]).parse(competitions_df)
 
+
+        # Connecting performances to competitions
         performances = PerformanceParser(cfg.columns["schedule"]).parse(schedule_df)
         for performance, competition_id in performances:
             competition = self.get_competition_by_id(competition_id)
@@ -43,8 +62,8 @@ class ScheduleGraph:
                 print(f"Competition with id {competition_id} not found")
             self.performances.append(performance)
 
+        # Connecting judges to competitions
         juries = JuryParser(cfg.columns["jury"]).parse(jury_df)
-
         for jury, assignments in juries:
             for competition_id in assignments:
                 competition = self.get_competition_by_id(competition_id)
@@ -55,20 +74,22 @@ class ScheduleGraph:
                 jury.performances.append(competition)
             self.juries.append(jury)
 
-        competitors = CompetitorsParser(cfg.columns["competitors"]).parse(registered_df)
-
-        for pairs in competitors:
-            competitor = pairs[0]
+        # Connecting competitors to competitions
+        dancers = CompetitorsParser(cfg.columns["competitors"]).parse(registered_df)
+        for pairs in dancers:
+            dancer = pairs[0]
             competitions = pairs[1]
+
 
             for competition_id in competitions:
                 competition = self.get_competition_by_id(competition_id)
                 if competition:
-                    competition.competitors.append(competitor)
+                    competition.competitors.append(dancer)
                 else:
                     print(f"Competition with id {competition_id} not found")
-                competitor.performances.append(competition)
-            self.competitors.append(competitor)
+                for performance in competition.performances:
+                    dancer.performances.append(performance)
+            self.competitors.append(dancer)
 
         print("Data filled")
 
