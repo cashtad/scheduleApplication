ff48b7ee281d5a450a1fe0adb298f9e20a9378eb
Leonid <lemalaxz2@gmail.com>
2025-11-17 11:02:31 +0100

    added visualization of graph

--- src/graph/main.py
+++ src/graph/main.py
@@ -1,15 +1,10 @@
 from src.graph.graph import ScheduleGraph
-
-
+from src.graph.graph_visualizer import ScheduleGraphVisualizer
 
 if __name__ == "__main__":
     graph = ScheduleGraph()
     graph.fill_with_data("C://Users/Leonid/PycharmProjects/scheduleApplication/src/config/config.yaml")
 
-    print("All performances of Viktorie Šerá:")
-
-    result = graph.get_performances_by_fullname("Viktorie Šerá")
-
-    for performance in result:
-        print(performance)
+    visualizer = ScheduleGraphVisualizer(graph)
+    visualizer.visualize()
 
