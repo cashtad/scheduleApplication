acd0f468883068009c354136615965a1f0ae6894
Leonid Malakhov <lemalaxz2@gmail.com>
2025-11-19 13:10:45 +0100

    dynamic path to cfg file

--- src/main.py
+++ src/main.py
@@ -1,9 +1,14 @@
+from pathlib import Path
+
 from src.graph.graph import ScheduleGraph
 from src.graph.graph_visualizer import ScheduleGraphVisualizer
 
 if __name__ == "__main__":
+    BASE_DIR = Path(__file__).parent  # /src
+    CONFIG_PATH = BASE_DIR / "config" / "config.yaml"
+
     graph = ScheduleGraph()
-    graph.fill_with_data("C://Users/Leonid/PycharmProjects/scheduleApplication/src/config/config.yaml")
+    graph.fill_with_data(str(CONFIG_PATH))
 
     visualizer = ScheduleGraphVisualizer(graph)
     visualizer.visualize()
