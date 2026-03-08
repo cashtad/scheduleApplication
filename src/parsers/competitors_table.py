cf558dba3962955083953b6c3e8d76533a244ebf
Leonid <lemalaxz2@gmail.com>
2026-03-08 18:11:52 +0100

    updated parsing avoiding mistakes

--- src/parsers/competitors_table.py
+++ src/parsers/competitors_table.py
@@ -10,25 +10,39 @@ class CompetitorsParser:
 
     def parse(self, df: pd.DataFrame) -> List[Any]:
         prefix = self._cols["assignment_prefix"]
-        competition_cols = [c for c in df.columns if c.startswith(prefix)]
+        competition_cols = [c for c in df.columns if str(c).startswith(prefix)]
+
+        filtered_competition_cols = set()
+
+        for col in competition_cols:
+            temp = str(col).strip().removeprefix(prefix)
+            for i, char in enumerate(temp):
+                if not char.isdigit():
+                    break
+                elif i == len(temp) - 1:
+                    filtered_competition_cols.add(col)
+
         result = []
         for i, row in df.iterrows():
             count = row[self._cols["count"]]
-            full_name_1 = row[self._cols["p1_name_surname"]].strip()
-            full_name_2 = None
-            if count == "2":
-                full_name_2 = row[self._cols["p2_name_surname"]].strip()
+            full_name_1 = str(row[self._cols["p1_name_surname"]].strip())
+            full_name_2 = str(row[self._cols["p2_name_surname"]].strip())  if count == "2" else None
+
             assignments = [
-                int(col.removeprefix(prefix))
-                for col in competition_cols
+                int(str(col).removeprefix(prefix))
+                for col in filtered_competition_cols
                 if str(row[col]).strip() == "1"
 
             ]
-            competitor = Competitor(
-                id=str(i),
-                count=int(count),
-                full_name_1=full_name_1,
-                full_name_2=full_name_2,
-            )
+            try:
+                competitor = Competitor(
+                    id=str(i),
+                    count=int(count),
+                    full_name_1=full_name_1,
+                    full_name_2=full_name_2,
+                )
+            except ValueError:
+                print(f"Invalid competitor count '{count}' for row {i}, skipping")
+                continue # skip rows with invalid count
             result.append((competitor, assignments))
         return result
