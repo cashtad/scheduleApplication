21fed559a18632f290da60786416e33fc04ab9f3
Leonid <lemalaxz2@gmail.com>
2026-03-27 14:23:53 +0100

    fixed inits

---
+++ src/ingestion/__init__.py
@@ -0,0 +1,4 @@
+from .contracts import IngestionIssue, TableParseStats, IngestionSeverity, FullIngestionResult, TableInput
+from .services import TableIngestionService
+
+__all__ = ['IngestionIssue', 'TableParseStats', 'IngestionSeverity', 'FullIngestionResult', 'TableInput', 'TableIngestionService']
