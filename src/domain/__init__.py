eea9ed4a454aea47671f7a4064ad39d791ab3dff
Leonid <lemalaxz2@gmail.com>
2026-03-27 13:50:19 +0100

    fixed inits

--- src/domain/__init__.py
+++ src/domain/__init__.py
@@ -1,1 +1,9 @@
-from .model import Human, Competitor, Competition, Performance, JuryMember
+from .model import Competitor, Competition, Performance, JuryMember
+from .schedule_repository import ScheduleRepository, ScheduleRepositoryBuilder, ScheduleRepositoryValidationReport
+
+__all__ = (["Competitor", "Competition", "Performance", "JuryMember"]
+           + [
+               "ScheduleRepository",
+               "ScheduleRepositoryBuilder",
+               "ScheduleRepositoryValidationReport",
+           ])
--- src/domain/schedule_repository/__init__.py
+++ src/domain/schedule_repository/__init__.py
@@ -1,19 +1,10 @@
-from .schedule_repository import ScheduleRepository, RepositoryBuildError
-from .schedule_repository_builder import ScheduleRepositoryBuilder, BuildScheduleRepositoryResult
+from .schedule_repository import ScheduleRepository
+from .schedule_repository_builder import ScheduleRepositoryBuilder
 from .schedule_repository_validation import (
-    ValidationIssueSeverity,
-    ScheduleRepositoryValidationIssue,
     ScheduleRepositoryValidationReport,
 )
-from .schedule_repository_validator import ScheduleRepositoryValidator
-
 __all__ = [
     "ScheduleRepository",
-    "RepositoryBuildError",
     "ScheduleRepositoryBuilder",
-    "BuildScheduleRepositoryResult",
-    "ScheduleRepositoryValidator",
-    "ValidationIssueSeverity",
-    "ScheduleRepositoryValidationIssue",
     "ScheduleRepositoryValidationReport",
 ]
