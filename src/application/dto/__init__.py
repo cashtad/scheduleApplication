5f2a5d98ea2ccc349ab488ee9b4d66f01445005c
Leonid <lemalaxz2@gmail.com>
2026-03-27 14:16:17 +0100

    fixed inits

--- src/application/dto/__init__.py
+++ src/application/dto/__init__.py
@@ -2,7 +2,7 @@ from .analyze_workflow_result import AnalyzeWorkflowResult
 from .build_repository_result import BuildRepositoryResult
 from .data_quality_report import DataQualityReport
 from .prepare_data_result import PrepareDataResult
-from .readiness import AnalyzeReadinessResult, ReadinessDecision, ReadinessReason
+from .readiness import AnalyzeReadinessResult, ReadinessDecision, ReadinessReason, ReadinessReasonSeverity
 from .workflow_status import WorkflowStatus
 
 __all__ = [
@@ -14,4 +14,5 @@ __all__ = [
     "ReadinessDecision",
     "ReadinessReason",
     "WorkflowStatus",
+    "ReadinessReasonSeverity",
 ]
