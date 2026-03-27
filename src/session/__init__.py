5d996ee0b5b85ac373f290811dd73cf7d2424eae
Leonid <lemalaxz2@gmail.com>
2026-03-27 14:23:59 +0100

    fixed inits

--- src/session/__init__.py
+++ src/session/__init__.py
@@ -1,6 +1,6 @@
-from .app_session import AppSession
+from .app_session import AppSession, REQUIRED_TABLE_KEYS
 from .table_runtime_state import TableRuntimeState
 from .table_status import TableStatus
 
-__all__ = ["AppSession", "TableRuntimeState", "TableStatus"]
+__all__ = ["AppSession", "TableRuntimeState", "TableStatus", "REQUIRED_TABLE_KEYS"]
 
