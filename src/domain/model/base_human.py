717a500770ccb63056621fc607c6dbb16e5549cb
Leonid <lemalaxz2@gmail.com>
2026-05-02 18:05:24 +0200

    changed structure and changed warnings/errors behavior

--- src/domain/model/base_human.py
+++ src/domain/model/base_human.py
@@ -10,7 +10,5 @@ class Human(ABC):
     competition_ids: FrozenSet[int]
 
     def __post_init__(self) -> None:
-        if not self.competition_ids:
-            raise ValueError("Pole 'competition_ids' nesmí být prázdné")
-        if any(cid <= 0 for cid in self.competition_ids):
+        if self.competition_ids and any(cid <= 0 for cid in self.competition_ids):
             raise ValueError("Všechna ID soutěží v 'competition_ids' musí být větší než nula")
